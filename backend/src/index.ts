f5ff431970713e12585eb6c0e7bbac3057107536
Javae Vittoni <javaevittoni@gmail.com>
2025-11-01 15:44:14 -0700

    database setup

---
+++ backend/src/index.ts
@@ -0,0 +1,42 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import session from "cookie-session";
+import { config } from "./config/app.config";
+import connectDatabase from "./config/database.config";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    session({
+        name: "session",
+        keys: [config.SESSION_SECRET],
+        maxAge: 24 * 60 * 60 * 1000,
+        secure: config.NODE_ENV === "production",
+        httpOnly: true,
+        sameSite: "lax",
+    })
+);
+
+app.use(
+    cors({
+        origin: config.FRONTEND_ORIGIN,
+        credentials: true,
+    })
+);
+
+app.get(`/`, (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({
+        message: "Test message"
+    });
+});
+
+app.listen(config.PORT, async () => {
+  console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
+  await connectDatabase();
+});
+
